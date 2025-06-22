3dc814965cd811c809f04fcce1f33be048f829a1
semaxio <kutchms@gmail.com>
2025-06-22 17:24:47 +0300

    Add: add buttons and counter for test telegram mini apps. border 5px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 'use client'
 
+import {useState} from "react";
+
 export default function Home() {
+  const [counter, setCounter]   = useState(0)
   return (
-    <div className="w-full h-full border border-blue-700 flex justify-center items-center">
+    <div className="w-full h-full border-[5px] border-blue-700 flex justify-center items-center flex-col gap-[35px]">
       <h1 className="text-amber-600 font-bold text-[60px]">Hello world</h1>
+      <span className="text-[45px] font-bold">{counter}</span>
+      <div className="flex gap-[25px]">
+        <button className="border-[2px] border-gray-800 w-[75px] h-[45px] text-[25px] font-bold
+        rounded-sm flex items-center justify-center bg-blue-400 active:opacity-75" onClick={() => setCounter(counter + 1)}>+1</button>
+        <button className="border-[2px] border-gray-800 w-[75px] h-[45px] text-[25px] font-bold
+        rounded-sm flex items-center justify-center bg-blue-400 active:opacity-75" onClick={() => setCounter(counter - 1)}>-1</button>
+      </div>
     </div>
   );
 }
